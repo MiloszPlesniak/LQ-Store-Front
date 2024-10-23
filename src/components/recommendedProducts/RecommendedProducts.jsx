70074d5d80096be98fb7a055c7fddd4f30f7d259
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2024-10-23 03:22:28 +0200

    add
    finish recomendet product slider

--- src/components/recommendedProducts/RecommendedProducts.jsx
+++ src/components/recommendedProducts/RecommendedProducts.jsx
@@ -1,5 +1,6 @@
 import styles from "./RecommendedProducts.module.scss";
-
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { useState } from "react";
 const RecommendedProducts = ({ array }) => {
   const {
     products,
@@ -11,27 +12,62 @@ const RecommendedProducts = ({ array }) => {
     products__brend,
     products__tast,
     products__listBox,
+    products__button,
   } = styles;
+  const [transform, setTransform] = useState(0);
+  const handleLeftClick = () => {
+    if (transform >= 0) {
+      setTransform((array.length - 2) * -150);
+    } else {
+      setTransform(transform + 150);
+    }
+  };
+  const handleRightClick = () => {
+    if (transform <= (array.length - 2) * -150) {
+      setTransform(0);
+    } else {
+      setTransform(transform - 150);
+    }
+  };
+  console.log(transform);
 
   return (
     <section className={products}>
       <h4>Produkty które mogą ci się spodobać:</h4>
-      <div className={products__listBox}>
-        <ul className={products__list}>
-          {array.map((item) => {
-            return (
-              <li className={products__item} key={item._id}>
-                <figure className={products__box}>
-                  <img className={products__img} src={item.imgUrl} alt="" />
-                  <figcaption className={products__textBox}>
-                    <h6 className={products__brend}>{item.brend}</h6>
-                    <p className={products__tast}>{item.tastName}</p>
-                  </figcaption>
-                </figure>
-              </li>
-            );
-          })}
-        </ul>
+      <div>
+        <IoIosArrowBack
+          style={{ width: "100px" }}
+          className={products__button}
+          onClick={handleLeftClick}
+        />
+        <div className={products__listBox}>
+          <ul
+            className={products__list}
+            style={{
+              transform: `translateX(${transform}px)`,
+              transition: "transform 0.3s",
+            }}
+          >
+            {array.map((item) => {
+              return (
+                <li className={products__item} key={item._id}>
+                  <figure className={products__box}>
+                    <img className={products__img} src={item.imgUrl} alt="" />
+                    <figcaption className={products__textBox}>
+                      <h6 className={products__brend}>{item.brend}</h6>
+                      <p className={products__tast}>{item.tastName}</p>
+                    </figcaption>
+                  </figure>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <IoIosArrowForward
+          style={{ width: "100px" }}
+          className={products__button}
+          onClick={handleRightClick}
+        />
       </div>
     </section>
   );
